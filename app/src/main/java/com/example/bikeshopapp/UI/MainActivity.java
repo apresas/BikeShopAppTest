65dd2be1a2473bf7184ec70955cb1f795ce51f2b
apresas <a.presas@outlook.com>
2022-02-21 15:44:17 -0800

    second commit

--- app/src/main/java/com/example/bikeshopapp/UI/MainActivity.java
+++ app/src/main/java/com/example/bikeshopapp/UI/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // add a line of code
     }
 
     public void enterHere(View view) {

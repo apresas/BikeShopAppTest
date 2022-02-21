42cc6218cc1d0b7fe86bf0a5a4e16c2839fc6a7b
apresas <a.presas@outlook.com>
2022-02-21 14:42:53 -0800

    First commit

---
+++ app/src/main/java/com/example/bikeshopapp/UI/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bikeshopapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bikeshopapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void enterHere(View view) {
+        Intent intent = new Intent(MainActivity.this, ProductList.class);
+        startActivity(intent);
+
+    }
+}
